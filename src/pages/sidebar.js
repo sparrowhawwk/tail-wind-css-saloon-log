b9cebca210a7f05e5217dfccc7425c655b9f342b
Charun <kingring23@hotmail.com>
2023-04-30 09:38:06 +0530

    Added table and css changes.

--- src/pages/sidebar.js
+++ src/pages/sidebar.js
@@ -1,11 +1,11 @@
 export default function Sidebar({ chooseComponent }) {
     return(
         <>
-            <ul className="list-none">
-                <li className="p-2 m-2">
+            <ul className="list-none space-y-2">
+                <li className="p-2 m-2 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                     <button onClick={() => chooseComponent('today')}>Today</button>
                 </li>
-                <li className="p-2 m-2">
+                <li className="p-2 m-2 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                     <button onClick={() => chooseComponent('history')}>History</button>
                 </li>
             </ul>
--- src/pages/today.js
+++ src/pages/today.js
@@ -1,6 +1,16 @@
 export default function Today() {
+    const tableCss = `
+        .table-wrp  {
+            max-height: 30vh;
+            overflow-y: auto;
+            display:block;
+        }
+    `;
     return (
         <>
+            <style>
+                {tableCss}
+            </style>
             <div className="p-2">
                 <div className="w-full">
                     <div className="flex mb-5">
@@ -44,6 +54,43 @@ export default function Today() {
                             <button className="rounded bg-green-500 border border-solid border-green-200 pt-1 pb-1 pr-4 pl-4 text-white">Log</button>
                         </div>
                     </div>
+                </div>
+                <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+                <div className="w-full overflow-x-auto table-wrp max-h-20">
+                    <table className="table-auto min-w-full text-left">
+                        <thead className="border-b dark:border-neutral-500 bg-neutral-50">
+                            <tr>
+                                <th scope="col" className="px-6 py-4">#</th>
+                                <th scope="col" className="px-6 py-4">Firstname</th>
+                                <th scope="col" className="px-6 py-4">Lastname</th>
+                                <th scope="col" className="px-6 py-4">Age</th>
+                                <th scope="col" className="px-6 py-4">Gender</th>
+                                <th scope="col" className="px-6 py-4">Amount</th>
+                                <th scope="col" className="px-6 py-4">Date</th>
+                            </tr>
+                        </thead>
+                        <tbody className="overflow-y-auto max-h-20">
+                            {
+                                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(
+                                    value => {
+                                        return (
+                                            <>
+                                                <tr className="border-b dark:border-neutral-500 ease-in-out hover:bg-neutral-100" key={value}>
+                                                    <td className="whitespace-nowrap px-6 py-4">1</td>
+                                                    <td className="whitespace-nowrap px-6 py-4">Rahul</td>
+                                                    <td className="whitespace-nowrap px-6 py-4">Sultan</td>
+                                                    <td className="whitespace-nowrap px-6 py-4">26</td>
+                                                    <td className="whitespace-nowrap px-6 py-4">Male</td>
+                                                    <td className="whitespace-nowrap px-6 py-4">110</td>
+                                                    <td className="whitespace-nowrap px-6 py-4">30/04/2023</td>
+                                                </tr>
+                                            </>
+                                        );
+                                    }
+                                )
+                            }
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </>
